e2a1c9ab9a384c307750a172a7c1938fa02e24f1
Wesley Moore <wes@wezm.net>
2012-01-30 07:51:44 +1100

    Cleanup attribute includes

--- src/attribute.cc
+++ src/attribute.cc
@@ -31,12 +31,8 @@ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#include <node.h>
-#include <node_events.h>
 #include <iostream>
 
-#include "genx.h"
-#include "node-genx.h"
 #include "attribute.h"
 
 using namespace v8;
